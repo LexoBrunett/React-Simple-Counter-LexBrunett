acd8690683706e2da62283797988cdfefe686d80
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-14 02:12:16 +0000

    Counter creationg and css

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,40 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { PropTypes } from "prop-types";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props){
+    return(<div className="bigCounter">
+        <div className="Card_container"><FontAwesomeIcon icon={faClock} /></div>
+        <div className = "Card_container"> <div className="cuartodigito">{props.DigitFour  % 10}</div> </div>
+        <div className = "Card_container"> <div className="tercerdigito">{props.DigitThree  % 100}</div> </div>
+        <div className = "Card_container"> <div className="segundodigito">{props.DigitTwo  % 10}</div> </div>
+        <div className = "Card_container"> <div className="primerdigito">{props.DigitOne  % 10}</div> </div>
+    </div>);
+}
+SimpleCounter.prototype = {
+    DigitFour: PropTypes.number,
+    DigitThree: PropTypes.number,
+    DigitTwo: PropTypes.number,
+    DigitOne: PropTypes.number
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval (function(){
+    const cuartodigito = Math.floor(counter/1000);
+    const tercerdigito = Math.floor(counter/100);
+    const segundodigito = Math.floor(counter/10);
+    const primerdigito = Math.floor(counter/1);
+    counter++;
+    ReactDOM.render(
+        <SimpleCounter DigitOne={primerdigito} DigitTwo={segundodigito} DigitThree={tercerdigito} DigitFour={cuartodigito} />,
+        document.querySelector("#app")
+    );
+
+
+},1000);    
+   
--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js']
   },
   devtool: "source-map",
   devServer: {
